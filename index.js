09f5440c27a49ef4590d330fbc837d7c373d4dd9
morri <morrisoneano@gmail.com>
2022-07-02 22:02:23 -0300

    Funcion y html

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function bienvenida(nombre,apellido) {
-    let saludo = "Bienvenido " + nombre + " "+  apellido_persona;
+function bienvenida(nombre, apellido) {
+    let saludo = "Bienvenido " + nombre + " " + apellido_persona;
     alert(saludo)
 }
+
+
+
 
 let nombre = prompt("Indique su nombre")
 let apellido_persona = prompt("Indique su apellido")
 
-bienvenida(nombre,apellido_persona)
+
+bienvenida(nombre, apellido_persona);
+let montoTotal = parseInt(prompt("Ingrese el total de dinero requerido"));
+
+let interes = 1.50;
+function calcINt(montoTotal, interes) {
+    return montoTotal * interes
+}
+
+let totalConInteres = calcINt(montoTotal,interes);
+alert("El total a abonar con intereses incluidos es de " + totalConInteres , "$");
