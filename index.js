8d063fe6aa0df35c30d0a8dcf82310a4a6de6e94
mati <morrisoneano@gmail.com>
2022-07-02 21:10:15 -0300

    a notebook

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-Funcion
+function bienvenida(nombre,apellido) {
+    let saludo = "Bienvenido " + nombre + " "+  apellido_persona;
+    alert(saludo)
+}
+
+let nombre = prompt("Indique su nombre")
+let apellido_persona = prompt("Indique su apellido")
+
+bienvenida(nombre,apellido_persona)
